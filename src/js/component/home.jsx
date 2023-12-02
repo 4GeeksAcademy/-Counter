3fc6ca63e616537fe9e94d6af903f43c333937c1
Garx1212 <113130182+Garx1212@users.noreply.github.com>
2023-12-02 03:05:41 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+function Home ({seconds})
+{
+	return (<div className="clock d-flex justify-content-center my-5 w-100 p-3 bg-dark text-dark">
+		
+		
+		<div className="px-2">
+		<div className="primero object-fit-none border rounded fs-1 px-2 bg-light ">{Math.floor(seconds / 100000) % 10}</div>
 		</div>
-	);
+
+		<div className="px-2">
+			<div className="segundo object-fit-none border rounded fs-1 px-2 bg-light">{Math.floor(seconds / 10000) % 10}</div>
+		</div>
+
+		<div className="px-2">
+			<div className="tercero object-fit-none border rounded fs-1 px-2 bg-light">{Math.floor(seconds / 1000) % 10}</div>
+		</div>
+
+		<div className="px-2">
+			<div className="Cuarto object-fit-none border rounded fs-1 px-2 bg-light">{Math.floor(seconds / 100) % 10}</div>
+		</div>
+
+		<div className="px-2">
+			
+			<div className="quinto object-fit-none border rounded fs-1 px-2 bg-light">{Math.floor(seconds / 10) % 10}</div>
+		</div>
+
+		<div className="px-2">
+			<div className="sexto object-fit-none border rounded fs-1 px-2 bg-light">{Math.floor(seconds / 1) % 10}</div>
+		</div>
+
+	</div>);
 };
 
+
 export default Home;
+
+
+
+
+
+
